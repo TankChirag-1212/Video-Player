aa5b3455a67e5b3481a772c2f1b55fcde38c9a14
TankChirag-1212 <106546348+TankChirag-1212@users.noreply.github.com>
2024-01-11 10:10:12 +0530

    Update theme.js

--- client/src/reducers/theme.js
+++ client/src/reducers/theme.js
@@ -1,15 +1,1 @@
-// const initialState = {
-//     theme: JSON.parse(localStorage.getItem('theme')) || true,
-// };
 
-// const themeReducer = ( state = initialState, action) => {
-//     switch (action.type) {
-//         case "SET_THEME":
-//             localStorage.setItem("theme", JSON.stringify(action.payload))
-//             return { ...state, theme: action.payload};
-//         default:
-//             return state;
-//     }
-// }
-
-// export default themeReducer;
